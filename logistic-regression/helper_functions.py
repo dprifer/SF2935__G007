df845363c5dc923c06e07763d1bf42f9b28dbb31
Tove Nordmark <tove.nordmark.01@gmail.com>
2024-10-10 16:14:04 +0200

    Fixed error relating to handling categorical variables, added documentation

--- logistic-regression/helper_functions.py
+++ logistic-regression/helper_functions.py
@@ -3,9 +3,31 @@ import pandas as pd
 
 
 def generate_train_test_split(df, vars_to_drop=[]):
+    """
+    This function generates a (seeded) 80/20 train test split for the spotify data with the categorical "key" and "mode" are
+    transformed to dummy variables, with no interaction terms included.
+
+    Params:
+    -------
+    df : pandas.DataFrame
+        The labels should match the original data frame of song data.
+    vars_to_drop : list
+        A list of labels containing variables not to be included in the model.
+
+    Returns:
+    --------
+    (pandas.DataFrame, pandas.DataFrame, pandas.Series, pandas.Series):
+        A sequence of four data frames/series with the first two being the training and testing predictors
+        and the last two being the labels of the training data and testing data.
+    """
 
     all_predictors = df.drop(vars_to_drop + ["Label"], axis=1)
     response = df["Label"]
 
-    encoded_data = pd.get_dummies(all_predictors, columns=["key", "mode"])
+    # We drop_first because one of the variables in each of key,
+    # mode is determined by the values of the others
+    # (for example mode != 0 implies mode = 1)
+    encoded_data = pd.get_dummies(
+        all_predictors, columns=["key", "mode"], drop_first=True
+    )
     return train_test_split(encoded_data, response, test_size=0.2, random_state=0)
